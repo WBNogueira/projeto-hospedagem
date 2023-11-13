e2d16bda1c466f99043f33962a79775f694f8023
Wagner Nogueira <wbnogueira3@gmail.com>
2023-11-13 16:42:38 -0300

    Complemento do desafio de projeto Hospedagem

---
+++ models/Pessoa.cs
@@ -0,0 +1,23 @@
+
+namespace projeto_hospedagem.models
+{
+    public class Pessoa
+    {
+        //public Pessoa() { }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+    }
+}
---
+++ models/Suite.cs
@@ -0,0 +1,19 @@
+
+namespace projeto_hospedagem.models
+{
+    public class Suite
+    {
+        //public Suite() { }
+
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+    }
+}
